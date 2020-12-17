12421728126225ec3148f75a6e7e847bde8ef345
adeSugiono <adesugiono354@gmail.com>
2020-12-17 17:33:19 +0700

    modif Phonebooks

--- client/src/containers/Phonebooks.js
+++ client/src/containers/Phonebooks.js
@@ -1,13 +1,14 @@
-import { connect } from 'react-redux'
-import { deletePhonebooks, resendPhonebooks } from '../actions'
-import ItemSiswa from '../components/ItemSiswa'
+import { connect } from 'react-redux';
+import { deletePhonebooks, resendPhonebooks, editPhonebooks } from '../actions';
+import TablePhone from './TablePhone';
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
     removeData: () => dispatch(deletePhonebooks(ownProps.id)),
-    resendData: () => dispatch(resendPhonebooks(ownProps.id, ownProps.name, ownProps.phone))
+    resendData: () => dispatch(resendPhonebooks(ownProps.id, ownProps.name, ownProps.phone)),
+    editData: (name, phone) => dispatch(editPhonebooks(ownProps.id, name, phone))
 })
 
 export default connect(
     null,
     mapDispatchToProps
-)(ItemSiswa)
+)(TablePhone)
