59b53997d5c81d6c1e6fa823ca4ca7134fb27568
adeSugiono <adesugiono354@gmail.com>
2020-12-17 17:49:40 +0700

    modif BoxPhone

--- client/src/components/BoxPhone.js
+++ client/src/components/BoxPhone.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import ButtonAdd from './ButtonAdd';
-import ListPhone from './ListPhone';
+import React from 'react';
+import ButtonAdd from '../containers/ButtonAdd';
+import ListPhone from '../containers/ListPhone';
 
 
 
 export default function BoxPhone(props) {
     return (
         <div className="container ">
-            <h1 className="card text-center alert alert-dark">Phone Book Apps</h1>
+            <h1 className="card text-center alert alert-dark"> <i class="fas fa-address-book">   Phone Book Apps</i></h1>
             <ButtonAdd />
             <br />
             <ListPhone />
